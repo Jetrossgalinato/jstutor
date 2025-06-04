f173e654acc1d7df4fa1a35aefe3bdc31e80f974
Jetross <jetrossneri07@gmail.com>
2025-06-04 12:36:07 +0800

    added initial code for Navbar and App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CheatCard from './components/CheatCard';
+import { cheatsheetData } from './data/cheatsheetData';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: '2rem' }}>
+      <h1>JavaScript Cheatsheet</h1>
+      {cheatsheetData.map((item, index) => (
+        <CheatCard key={index} item={item} />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
